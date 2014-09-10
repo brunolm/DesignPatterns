7c601c492bac74691c9479d7ab2b53c2e57396f5
brunolm7 <brunolm7@gmail.com>
2014-09-10 20:11:55 -0300

    Fix typo

--- DesignPatterns.Observer/IDragonSubject.cs
+++ DesignPatterns.Observer/IDragonSubject.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DesignPatters.Observer
+namespace DesignPatterns.Observer
 {
     public interface IDragonSubject
     {
--- DesignPatterns.Observer/Properties/AssemblyInfo.cs
+++ DesignPatterns.Observer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DesignPatters.Observer")]
+[assembly: AssemblyTitle("DesignPatterns.Observer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DesignPatters.Observer")]
+[assembly: AssemblyProduct("DesignPatterns.Observer")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
